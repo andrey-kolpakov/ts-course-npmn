6e2321288075ba4a971b60db8b5e581918f7f18f
Andrew <gfstels@yandex.ru>
2025-01-17 10:50:16 +0500

    new

---
+++ dist/01-primitives.js
@@ -0,0 +1,8 @@
+"use strict";
+//numbers
+let x = 10.5;
+let y = 10;
+//string
+let str1 = 'Hello world!';
+let str2;
+str2 = 'Hello World';
---
+++ src/01-primitives.ts
@@ -0,0 +1,27 @@
+//numbers
+let x = 10.5
+let y = 10
+
+//string
+let str1: string = 'Hello world!'
+
+let str2: string
+str2 = 'Hello World'
+
+// boolean
+let o = true
+let a = false
+
+//nothing
+let g = undefined
+let s = null
+let p: undefined = undefined
+
+// literal
+const num = 108
+const str3 = 'str'
+
+//universal
+let mm: any = 1
+mm = 'str'
+mm = [1, 2, 3]
